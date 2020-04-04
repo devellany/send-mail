7fbc7898d82fa2bab3451460ff2108df5d01ed27
devellany <devellany@gmail.com>
2020-04-04 15:32:07 +0900

    GitHub Actions for send mail.

---
+++ README.md
@@ -0,0 +1,51 @@
+# send-mailer
+ - This is GitHub cusotem action.
+ - Dynamically send an email with attachments.
+
+##Sample .yml
+~~~yaml
+- name: Send email
+  uses: devellany/send-mailer@v1.0.0
+  with:
+    host: smtp.google.com
+      # Optional port (defaults to 465)
+      port: 465
+      account: ${{secrets.ACCOUNT}}
+      password: ${{secrets.MAIL_PASSWORD}}
+      sender: devellany
+      from: ${{secrets.MAIL_ADDRESS}}
+      to: devellay@dico.me
+      subject: mail title.
+      body: your messages.
+      # Optional content type (defaults to text/plain)
+      contentType: text/plain
+      # Optional attachment files (JSON type. require property 'path')
+      attachments: '[{"path":"README.md"}]'
+~~~
+
+## How to Secrets.KEY
+![how to secrets.KEY](src/secrets.png)
+
+## What is 'Dynamically' send an email?
+ Some properties can read files. The prefix 'file://' if you want to read a file.
+### Applicable properties
+ - to
+ - subject
+ - body
+ - contetnType
+ - attachments
+ ```yaml
+- name: Send email
+  uses: devellany/send-mailer@v1.0.0
+  with:
+    host: smtp.google.com
+      account: ${{secrets.ACCOUNT}}
+      password: ${{secrets.MAIL_PASSWORD}}
+      sender: devellany
+      from: ${{secrets.MAIL_ADDRESS}}
+      to: devellay@dico.me
+      subject: mail title.
+      body: file://README.md
+      contentType: text/html
+      attachments: file://attachmets.json
+```
---
+++ main.js
@@ -0,0 +1,71 @@
+const sendMail = (() => {
+    const core = require('@actions/core');
+    const nodemailer = require('nodemailer');
+    const fs = require('fs');
+
+    function getInputData() {
+        return {
+            host: core.getInput("host", {required: true}),
+            port: parseInt(core.getInput("port", {required: false})),
+            account: core.getInput("account", {required: true}),
+            password: core.getInput("password", {required: true}),
+            from: `${core.getInput("sender", {required: true})} <${core.getInput("from", {required: true})}>`,
+            to: readData(core.getInput("to", {required: true})),
+            subject: readData(core.getInput("subject", {required: true})),
+            body: readData(core.getInput("body", {required: true})),
+            contentType: readData(core.getInput("contentType", {required: true})),
+            attachments: JSON.parse(readData(core.getInput("attachments", {required: false}))),
+        };
+    }
+
+    function readData(data) {
+        const prefix = 'file://';
+        if (!data.startsWith(prefix)) {
+            return data;
+        }
+
+        return fs.readFileSync(data.replace(prefix, ''), 'utf8');
+    }
+
+    function isSecure(port) {
+        return port === 465;
+    }
+
+    function getBodyKey(contentType) {
+        return contentType === 'text/html' ? 'html' : 'body';
+    }
+
+    function validAttachments(attachments) {
+        return  attachments.filter(row => {
+            return typeof row.path !== 'undefined';
+        });
+    }
+
+    return {
+        start: () => {
+            try {
+                const data = getInputData();
+
+                nodemailer.createTransport({
+                    host: data.host,
+                    port: data.port,
+                    secure: isSecure(data.port),
+                    auth: {
+                        user: data.account,
+                        pass: data.password,
+                    }
+                }).sendMail({
+                    from: data.from,
+                    to: data.to,
+                    subject: data.subject,
+                    [getBodyKey(data.contentType)]: data.body,
+                    attachments: validAttachments(data.attachments)
+                });
+            } catch (e) {
+                core.setFailed(e.message)
+            }
+        }
+    };
+})();
+
+sendMail.start();
